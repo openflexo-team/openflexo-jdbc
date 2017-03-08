c94b7c692d97483f077804dede1ad46eb62cae67
Jean-Charles <jeancharles.roger@me.com>
2017-03-08 16:05:11 +0100

    LOW / Corrects actor reference

--- jdbcconnector/src/main/java/org/openflexo/technologyadapter/jdbc/fml/JDBCActorReference.java
+++ jdbcconnector/src/main/java/org/openflexo/technologyadapter/jdbc/fml/JDBCActorReference.java
@@ -36,9 +36,9 @@
 package org.openflexo.technologyadapter.jdbc.fml;
 
 import org.openflexo.foundation.fml.annotations.FML;
-import org.openflexo.foundation.fml.rt.AbstractVirtualModelInstance;
 import org.openflexo.foundation.fml.rt.ActorReference;
 import org.openflexo.foundation.fml.rt.ModelSlotInstance;
+import org.openflexo.foundation.resource.ResourceData;
 import org.openflexo.model.annotations.ImplementationClass;
 import org.openflexo.model.annotations.ModelEntity;
 import org.openflexo.model.annotations.XMLElement;
@@ -53,14 +53,17 @@ import org.openflexo.technologyadapter.jdbc.model.JDBCConnection;
 @ImplementationClass(JDBCActorReference.JDBCActorReferenceImpl.class)
 @FML("JDBCActorReference")
 public interface JDBCActorReference<T> extends ActorReference<T> {
+
 	abstract class JDBCActorReferenceImpl<T> extends ActorReferenceImpl<T> implements JDBCActorReference<T>  {
 		public JDBCConnection getConnection() {
-			// FIXME How do I retrieve the JDBCConnection ?
-			AbstractVirtualModelInstance<?, ?> virtualModelInstance = getVirtualModelInstance();
-			if (virtualModelInstance == null) return null;
-
-			ModelSlotInstance<?, JDBCConnection> modelSlotInstance = virtualModelInstance.getModelSlotInstance("db");
-			return modelSlotInstance.getAccessedResourceData();
+			ModelSlotInstance<?, ?> modelSlotInstance = getModelSlotInstance();
+			if (modelSlotInstance != null) {
+				ResourceData<?> resourceData = modelSlotInstance.getAccessedResourceData();
+				if (resourceData instanceof JDBCConnection) {
+					return (JDBCConnection) resourceData;
+				}
+			}
+			return null;
 		}
 	}
 }
